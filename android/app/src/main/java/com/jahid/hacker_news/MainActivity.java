9427da1d02ee24aa56f4234ecff77b0dde51acd4
jahiduloo7 <jahidul479@gmail.com>
2021-01-26 12:27:43 +0600

    route

---
+++ android/app/src/main/java/com/jahid/hacker_news/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jahid.hacker_news;
+import android.content.Intent;
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+
+
+
+}
